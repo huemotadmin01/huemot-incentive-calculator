140d993a624caef8fa8c906570ca6dedbe3b33d6
huemotadmin01 <priyanshu.sahu@huemot.com>
2025-11-09 12:53:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ==========================================
 // CONFIGURATION
 // ==========================================
-const API_URL = 'YOUR_APPS_SCRIPT_WEB_APP_URL_HERE'; // Replace after deploying Apps Script
+const API_URL = 'https://script.google.com/macros/s/AKfycbym7p3d8KDor4eNUqt2vCE9Gr86I9DUYg47fnBfiuwFVqIen5VSexYssR5VX6ZfT5Hf/exec'; // Replace after deploying Apps Script
 
 // Application State
 let appState = {
